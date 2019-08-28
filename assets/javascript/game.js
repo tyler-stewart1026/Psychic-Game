8d7b4d40afa4943105a1d70cd24bfbe84ad3d3af
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-08-27 21:39:11 -0600

    framework for psychic assignment. good start I think

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//some variables
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var guessesSoFar = 0;
+var selectedLetter = 0;
+
+var computerNumber = getRandomLetter();
+
+//some functions
+function getRandomLetter() {
+    var RandLttr = Math.floor(Math.random()*charList.length);
+    return rand;
+}
+
+function checkUserGuess() {}
+
+function displayLettersGuessed() {}
+
+function reset() {}
+
+
+
+
+
+
+
+
+document.onkeyup = function (event) { 
+
+function randomNumber() {
+    var text = " "
+    var charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < l; i++) {
+        text += charList.charAt(Math.floor(Math.random() * char_list.length));
+    }
+    return text;
+}
+console.log(randomNumber(1));
+
